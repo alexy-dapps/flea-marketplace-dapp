21909751a35b471aecfabc3b6f02bab6c5849046
alexytiger <yevseyevich@msn.com>
2019-12-02 00:06:14 -0500

    dig

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  console.log('Accounts', accounts);
   deployer.deploy(Migrations);
 };
 
