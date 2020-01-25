bd5a09256237f8aadf745068372476f26acd784a
alexytiger <yevseyevich@msn.com>
2020-01-24 20:55:26 -0500

    dig

--- ClientApp/src/app/core/store/effects/web3-provider.effects.ts
+++ ClientApp/src/app/core/store/effects/web3-provider.effects.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { serializeError } from 'serialize-error';
 import { Router, ActivatedRouteSnapshot } from '@angular/router';
 import { of, from, fromEvent, EMPTY as empty } from 'rxjs';
-import { exhaustMap, switchMap, map, tap, catchError, withLatestFrom } from 'rxjs/operators';
+import { exhaustMap, switchMap, map, tap, catchError, withLatestFrom, filter } from 'rxjs/operators';
 
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
@@ -22,6 +22,9 @@ export class Web3ProviderEffects {
     private providerSrv: EthersProviderService,
   ) { }
 
+
+  // ethereum.enable() will be Deprecated
+  /*
   metaMaskEnable$ = createEffect(() =>
     this.actions$.pipe(
       ofType(Web3ProviderActions.init),
@@ -55,6 +58,35 @@ export class Web3ProviderEffects {
         }
 
         return empty;
+      })
+    )
+  );
+  */
+
+  metaMaskEnable$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(Web3ProviderActions.init),
+      exhaustMap(() => {
+
+        // Requests the user provides an ethereum address to be identified by.
+        // Returns a promise of an array of hex-prefixed ethereum address strings.
+        // Currently only ever one: accounts[0], for example,
+        // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
+        return from(this.web3Token.send('eth_requestAccounts')).pipe(
+          tap((ethAccounts: string[]) =>
+            console.log(`Ethereum provider has been granted access to the following accounts: ${ethAccounts}`)
+          ),
+          map((ethAccounts: string[]) => {
+            if (ethAccounts.length === 0) {
+              return ErrorActions.errorMessage({ errorMsg: `Can't get any user accounts` });
+            }
+            return Web3ProviderActions.initSuccess();
+          }),
+          // User denied account access
+          catchError((err: Error) => of(this.handleError(err), SpinnerActions.hide())
+          )
+        );
+
       })
     )
   );
@@ -138,23 +170,26 @@ export class Web3ProviderEffects {
   // event, Event {isTrusted: true, type: "resize", target: Window, currentTarget: Window, eventPhase: 2, …}
   */
 
- // based on https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1193.md#chainchanged
- // and https://gist.github.com/rekmarks/d318677c8fc89e5f7a2f526e00a0768a
+  // based on https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1193.md#chainchanged
+  // and https://gist.github.com/rekmarks/d318677c8fc89e5f7a2f526e00a0768a
+  // Note that this event is emitted on page load also
+ // If the array of accounts is non-empty, you're already connected.
   accountChanged$ = createEffect(
     () =>
-    fromEvent(this.web3Token, 'accountsChanged').pipe(
-      withLatestFrom(this.store$.pipe(select(fromStore.getAccount))),
-      tap(([accounts, currentAccount])  => {
+      fromEvent(this.web3Token, 'accountsChanged').pipe(
+        withLatestFrom(this.store$.pipe(select(fromStore.getAccount))),
+        // filter(([accounts, currentAccount]) => ((accounts as Array<string>).length > 0 && !!currentAccount)),
+        tap(([accounts, currentAccount]) => {
 
-         if (currentAccount !== accounts[0]) {
-            console.log( 'new account', accounts[0]);
+          if (currentAccount !== accounts[0]) {
+            console.log('new account', accounts[0]);
             // we need to reload browser
             // based onhttps://medium.com/metamask/no-longer-reloading-pages-on-network-change-fbf041942b44
             document.location.reload();
-         }
-      })
+          }
+        })
       ),
-      { dispatch: false }
+    { dispatch: false }
   );
 
 
