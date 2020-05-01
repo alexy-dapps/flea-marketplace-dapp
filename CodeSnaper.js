6e9b15f6c18788e27d9b306a8063a8f1114d351e
Alex Tiger <alexanddanik@gmail.com>
2020-04-30 20:55:18 -0400

    dig

--- CodeSnaper.js
+++ CodeSnaper.js
@@ -1,7 +1,14 @@
-export interface Web3GatewayState {
-  ethereumInjected: boolean;
-  ethereumConnected: boolean;  
-  account: string;
-  ethereumNetwork: string;
-  balance: string;
+ngOnInit() {
+  this.network$ = this.store.pipe(select(fromRoot.getNetwork));
+  this.balance$ = this.store.pipe(select(fromRoot.getBalance));
+}
+
+ngAfterViewInit() {
+
+  this.account$ = this.store.pipe(select(fromRoot.getAccount)).pipe(
+    tap(account => {
+      this.blockyRef.nativeElement.src =
+        create({ seed: account, size: 8, scale: 4 }).toDataURL();
+    })
+  );
 }
