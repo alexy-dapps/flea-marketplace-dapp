b696542b214d6525312c4f590f701970269f481b
Alex Tiger <alexanddanik@gmail.com>
2020-05-03 19:24:56 -0400

    dig

--- ClientApp/src/app/market-place/components/show-ipfs-image/show-ipfs-image.component.ts
+++ ClientApp/src/app/market-place/components/show-ipfs-image/show-ipfs-image.component.ts
@@ -1,5 +1,5 @@
 
-import { ChangeDetectionStrategy, Component, Inject, OnInit, ViewChild , ElementRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { windowRefToken } from '../../../core/services/tokens';
 import { Observable } from 'rxjs';
@@ -17,21 +17,21 @@ import * as IpfsActions from '../../store/actions/ipfs-product-image.actions';
 })
 export class ShowIpfsImageComponent implements OnInit {
 
-  @ViewChild('ipfsImage', {static: true}) imageRef: ElementRef;
+  @ViewChild('ipfsImage', { static: true }) imageRef: ElementRef;
   image$: Observable<Blob>;
 
   constructor(
-        private store$: Store<fromPurchaseContract.AppState>,
-        public dialogRef: MatDialogRef<ShowIpfsImageComponent>,
-        @Inject(windowRefToken) private windowRef: Window
-        ) { }
+    private store$: Store<fromPurchaseContract.AppState>,
+    public dialogRef: MatDialogRef<ShowIpfsImageComponent>,
+    @Inject(windowRefToken) private windowRef: Window
+  ) { }
 
 
   ngOnInit() {
     this.image$ = this.checkStore().pipe(
       tap((blob) =>
-          this.imageRef.nativeElement.src = this.windowRef.window.URL.createObjectURL(blob)
-       )
+        this.imageRef.nativeElement.src = this.windowRef.window.URL.createObjectURL(blob)
+      )
     );
   }
 
@@ -40,23 +40,23 @@ export class ShowIpfsImageComponent implements OnInit {
       select(fromPurchaseContract.getImageBlob),
       withLatestFrom(this.store$.pipe(select(fromPurchaseContract.getIpfsHash))),
 
-      tap( ([image, ipfsHash]) => {
+      tap(([image, ipfsHash]) => {
         if (!image) {
-          this.store$.dispatch(IpfsActions.downloadImage({ipfsHash}));
+          this.store$.dispatch(IpfsActions.downloadImage({ ipfsHash }));
         }
       }),
       map(([image, ipfsHash]) => image),
       // Notice that the filter() returns the observable sequence that contains elements
-        // from the input sequence that satisfy the condition.
-        // so in this case, if the image blob is null, the steam will not continue, but
-        // when the image blob is not empty, we grab this value.
-        // Which means we are waiting for the
-        // image blob value has become not null and then we continue the stream and take this one value .
-        // after that the whole stream will be completed.
+      // from the input sequence that satisfy the condition.
+      // so in this case, if the image blob is null, the steam will not continue, but
+      // when the image blob is not empty, we grab this value.
+      // Which means we are waiting for the
+      // image blob value has become not null and then we continue the stream and take this one value .
+      // after that the whole stream will be completed.
       filter(image => !!image),
       take(1)
     );
- }
+  }
 
 
 
